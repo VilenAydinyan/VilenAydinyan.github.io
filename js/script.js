8bd9e05d70ab62cb42370732b9698ed8a47934fa
VilenAydinyan <aydinyanvilen@gmail.com>
2023-02-22 12:16:33 +0400

    end

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  window.onclick = function(e) {
-    if (!e.target.matches('.dropbtn')) {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function (e) {
+  if (!e.target.matches(".dropbtn")) {
     var myDropdown = document.getElementById("myDropdown");
-      if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
-      }
+    if (myDropdown.classList.contains("show")) {
+      myDropdown.classList.remove("show");
     }
   }
+};
 
-  function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-  }
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
